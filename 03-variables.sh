9aa4a4e7ce03fed60ab885d3b2efd2419418db5e
A   Y    A   S <alluru.yasmine@gmail.com>
2023-12-13 19:46:48 +0530

    learning variables in shell script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please provide username:"
+read -s USERNAME
+echo "please provide password:"
+read -s PASSWORD
+
+echo " I am logging into amazon instance with username: $USERNAME and password: $PASSWORD "
