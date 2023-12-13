e90891936fa09b7f687014a2a4e87cd6c9b8907f
A   Y    A   S <alluru.yasmine@gmail.com>
2023-12-13 19:20:29 +0530

    learning variables in shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DATE=$(date)
-COST=$1
-CUSSWORD=$2
-CITY=$3
+CITY=$1
+COST=$2
+CUSSWORD=$3
 
 echo "I have a bought a house in $CITY costed about Rs. $COST excluding registration charges. I am completely $CUSSWORD!"
