19cbd2caf3dd35966959615764fded2ef55f1567
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 16:02:17 +0530

    practicing

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,9 +2,9 @@
 
 FRUITS=("apple" "banana" "mango")
 
-echo "my fav fruit is $(FRUITS[0])
+echo "my fav fruit is ${FRUITS[0]}"
 
-echo "my 2nd fav fruit is $(FRUITS[1])
+echo "my 2nd fav fruit is ${FRUITS[1]}"
 
-echo "my all time fav fruit is #(FRUITS[2])
+echo "my all time fav fruit is ${FRUITS[2]}"
 
