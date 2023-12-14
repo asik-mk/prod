3d32fd0fd3cd4ee0a179dcc6cceae7fc82a12319
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 19:26:32 +0530

    practicing

--- 09-logs.sh
+++ 09-logs.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 ID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Installation of $2 failed"
+        exit 1
+    else
+        echo "Successfully installed $2"
+    fi
+}
+
     if [ $ID -ne 0 ]
     then
         echo "Switch to root user"
@@ -9,22 +20,13 @@ ID=$(id -u)
     fi
 
 
+
 yum install mysql -y 
 
-    if [ $? -ne 0 ]
-    then
-        echo "Installation of mysql failed"
-        exit 1
-    else
-        echo "Successfully installed mysql"
-    fi
+VALIDATE $? mysql
 
 yum install git -y 
 
-    if [ $? -ne 0 ]
-    then
-        echo "Installation of git failed"
-        exit 1
-    else
-        echo "Successfully installed git"
-    fi
+VALIDATE $? git
+
+    
