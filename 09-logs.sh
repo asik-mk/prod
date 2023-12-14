0b9472d53a24b4f76ac55dbbeeba71f17149f17f
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 19:05:31 +0530

    practicing

--- 09-logs.sh
+++ 09-logs.sh
@@ -9,3 +9,12 @@ yum install mysql -y
     else 
         echo "Installing mysql"
     fi
+
+yum install git -y 
+
+    if [ $ID -ne 0 ]
+    then
+        echo "Switch to root user"
+    else 
+        echo "Installing git"
+    fi 
