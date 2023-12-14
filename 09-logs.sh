642955ad61e8e88e322597b208d364a2e93541a6
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 19:13:51 +0530

    practicing

--- 09-logs.sh
+++ 09-logs.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 ID=$(id -u)
 
-yum install mysql -y
+VALIDATE()
+    {
+        if [ $1 -ne 0 ]
+        then
+            echo "Installation of $2 failed"
+            exit 1
+        else 
+            echo "Installing $2"
+        fi
+    }
 
     if [ $ID -ne 0 ]
     then
         echo "Switch to root user"
+        exit 1
     else 
-        echo "Installing mysql"
+        echo "Loading the dock..."
     fi
+
+yum install mysql -y
+
+VALIDATE $? mysql
 
 yum install git -y 
 
-    if [ $ID -ne 0 ]
-    then
-        echo "Switch to root user"
-    else 
-        echo "Installing git"
-    fi 
+VALIDATE $? git
+
+yum install httpd -y 
+
+VALIDATE $? httpd
+ 
