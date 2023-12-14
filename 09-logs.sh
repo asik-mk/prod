626c928a3f6fa9c83dab5cd8f6752c420f1747ee
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 21:56:51 +0530

    practicing

--- 09-logs.sh
+++ 09-logs.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%h-%m-%s)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Installation of $2 failed"
+        echo "Installation of $2 $R failed $N"
         exit 1
     else
-        echo "Successfully installed $2"
+        echo "Successfully installed $G $2 $N"
     fi
 }
-
     if [ $ID -ne 0 ]
     then
-        echo "Switch to root user"
+        echo "Switch to $G root $N user"
         exit 1
     else
-        echo "fasten the belt"
+        echo "$R fasten $Y the $G belt $N"
     fi
-
-
 
 yum install mysql -y &>> $LOGFILE
 
