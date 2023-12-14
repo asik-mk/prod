25bebf1ff33917ba7fca29d5ac8868eac183a8f5
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 18:53:16 +0530

    practicing

--- 09-logs.sh
+++ 09-logs.sh
@@ -22,11 +22,11 @@ VALIDATE()
 
     if [ $1 -ne 0 ]
     then 
-            echo "installation of $2 $R failed $N"
+            echo -e "installation of $2 $R failed $N"
                 exit 1
     else
 
-        echo "Successfully $G installed $N $2"
+        echo -e "Successfully $G installed $N $2"
 
     fi
 
