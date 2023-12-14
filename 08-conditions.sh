8d2a8b907ff06c1d9083e0afd75e073dd534ba2f
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-14 16:54:34 +0530

    practicing

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 ID=(id -u)
-
-VALIDATE()
-    {
-        if [ $1 -ne 0 ]
-        then
-        
-            echo "switch to root user by running 'sudo su -' "
-            exit 1
-        else
-
-            echo "Successfully installed mysql"
-
-        fi
-
-    }
-
 yum install mysql -y
 
-VALIDATE $? "Installing"
+ if [ $? -ne 0 ]
+ then 
+        echo "switch to root user by running 'sudo su -' "
+            exit 1
+ else
+
+     echo "Successfully installed mysql"
+
+ fi
+
+
