edf05edcf5df57272c7df5d264766e8809063191
Mohammed Asik <k.mohammed-asik@capgemini.com>
2023-12-15 15:21:32 +0530

    package installation with loops

--- 11-packages.sh
+++ 11-packages.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 ID=$(id -u)
-TIMESTAMP=$(date +%y%m%d-%H%M%S)
-LOGFILE="/tmp/$0.$TIMESTAMP.log"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP-log"
+
+if [ ID -ne 0 ]
+then
+    echo " Switch to root user "
+    exit 1
+else 
+    echo " Proceeding "
+fi
 
 VALIDATE(){
 
-    if [ $1 -ne 0 ]
+    if [ $1 -ne 0]
     then
-        echo "$2 installation failed"
+        echo " $2 installation failed/already exists "
         exit 1
-    else
-        echo "$2 successfully installed"
+    else 
+        echo " $2 installed now "
     fi
-
 }
 
-if [ $ID -ne 0 ]
-then    
-    echo "Switch to root user"
-        exit 1
+
+yum list installed mysql
+
+if [ $? -ne 0 ]
+then
+    yum install mysql -y
 else
-    echo "FOR loop begins"
+    echo " mysql already Installed "
 fi
 
-for package in $@
-do
-        yum list installed $package
-        if [ $? -ne 0 ]
-        then 
-            yum install $package -y
-            VALIDATE $? "Installing the $package"
-        else
-            echo "Already installed"
-        fi
-done
+VALIDATE $? mysql
+
+yum list installed httpd
+
+if [ $? -ne 0 ]
+then
+    yum install httpd -y
+else
+    echo " httpd already installed "
+fi
+
+VALIDATE $? httpd
